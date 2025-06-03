55c77949b00db3bb08bbe832390cf9d12fb20639
Krystal Davison <kdaviso5@kent.edu>
2025-06-03 17:18:50 +0000

    new changes

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,14 @@
-from flask import Flask, redirect, url_for, request, flash, session, render_template, jsonify
+ffrom flask import Flask, redirect, url_for, request, flash, session, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import os
 from werkzeug.security import generate_password_hash, check_password_hash
-from forms import UpdateProfileForm
+from forms import UpdateProfileForm, UpdateWorkoutForm
 import secrets
 from PIL import Image 
+from datetime import datetime, timezone, timedelta
+from zoneinfo import ZoneInfo
+from flask_migrate import Migrate
+from sqlalchemy import func
 
 app = Flask(__name__)
 #this protects session data and hard-coding a secret key is risky
@@ -12,6 +16,7 @@ app.secret_key = os.urandom(24)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 #table to keep track of users friends 
 friends= db.Table('friends',
@@ -27,6 +32,9 @@ class User(db.Model):
     email = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(100), unique=False, nullable=False)
     image_file = db.Column(db.String(50), unique=False, nullable=False, default='default.png')
+    timezone = db.Column(db.String(50), nullable=True)
+    workouts = db.relationship('Workout', backref='user', lazy=True) 
+   
     friends = db.relationship('User', secondary=friends, primaryjoin=(friends.c.user_id == id),
         secondaryjoin=(friends.c.friend_id == id), backref=db.backref('friend_of', lazy='dynamic'), lazy='dynamic')
     #secondary=friends: uses the friends table to associate the users
@@ -44,6 +52,34 @@ class FriendRequest(db.Model):
 
     requester = db.relationship('User', foreign_keys=[requester_id], backref='sent_requests')
     receiver = db.relationship('User', foreign_keys=[receiver_id], backref='received_requests')
+
+#Model for workouts
+class Workout(db.Model):  
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    exercise = db.Column(db.String(50), nullable=False)
+    duration_minutes = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc)) #utc is current universal time
+    workout_title = db.Column(db.Text)
+
+
+#General Awards
+class Award(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(255))
+    icon = db.Column(db.String(100)) 
+
+#Awards for the user
+class UserAward(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    award_id = db.Column(db.Integer, db.ForeignKey('award.id'), nullable=False)
+    date_awarded = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc))
+    
+    #creates many to many relationship  
+    user = db.relationship('User', backref='user_awards')
+    award = db.relationship('Award', backref='user_awards')
 
 with app.app_context(): #needed for db operation
        db.create_all()
@@ -91,13 +127,108 @@ def logout():
 
 @app.route("/user_page", methods=["POST", "GET"])
 def user_page():
-    if "user" in session:
-        return render_template ("user_page.html", user=session["user"])
-    #not logged in
-    else:
+    if "user" not in session:
         flash("You are not logged in")
         return redirect(url_for("login"))
     
+    user_id = session["user"]
+    user = User.query.get(user_id)
+    image_file = url_for('static', filename='profile_pics/' + user.image_file)
+
+    
+    return render_template("user_page.html", user=user, image_file=image_file)
+
+
+@app.route("/user_page_api", methods=["POST", "GET"])
+def user_page_api():
+    #not logged in
+    if "user" not in session:
+        flash("You are not logged in")
+        return redirect(url_for("login"))
+    
+    # Get the full User object
+    user_id = session["user"]
+    user = User.query.get(user_id) 
+
+    #total activites, active minutes, and streak code
+    if user.workouts:
+        #select everything from workout where user_id=user_id .count()
+        total_workouts = Workout.query.filter_by(user_id=user_id).count()
+        #start a query, perform sum function, filter this by userID
+        #have to use db.session instead of workout.query to handle SQL functions. Need scalar to return a number
+        total_minutes = db.session.query(func.sum(Workout.duration_minutes)).filter_by(user_id=user_id).scalar()
+        total_minutes = round(total_minutes, 2)
+        #streak code
+        #this will count how many days someone exercised and reset when a day is missed
+        #select every workout where user_id=user_id and grab the date in descending order
+        dates = Workout.query.filter_by(user_id=user_id).with_entities(Workout.date).order_by(Workout.date.desc()).all()
+
+        
+        workout_days = set(date[0].date() for date in dates)
+
+        today = datetime.now(timezone.utc).date()
+        day = today
+        streak = 0
+
+        # If no workout today, skip it once
+        if day not in workout_days:
+            day -= timedelta(days=1)
+
+        # Count consecutive days with workouts
+        while day in workout_days:
+            streak += 1
+            day -= timedelta(days=1)
+    else: 
+        total_workouts = 0
+        total_minutes = 0
+        streak = 0
+    #total awards code
+    awards = UserAward.query.filter_by(user_id=user_id).count()
+
+    return jsonify(streak=streak, total_workouts=total_workouts, total_minutes=total_minutes, awards=awards)
+
+@app.route("/view_other_user/<int:user_id>", methods=["POST", "GET"])
+def view_other_user(user_id):
+    user = User.query.get(user_id)
+    image_file = url_for('static', filename='profile_pics/' + user.image_file)
+
+    return render_template("view_other_user.html", user=user, image_file=image_file)
+
+@app.route("/view_other_user_api/<int:user_id>", methods = ["POST", "GET"])
+def view_other_user_api(user_id):
+    user = User.query.get(user_id) 
+
+    #total activites, active minutes, and streak code
+    if user.workouts:
+        total_workouts = Workout.query.filter_by(user_id=user_id).count()
+        total_minutes = db.session.query(func.sum(Workout.duration_minutes)).filter_by(user_id=user_id).scalar()
+        total_minutes = round(total_minutes, 2)
+
+        #streak code
+        dates = Workout.query.filter_by(user_id=user_id).with_entities(Workout.date).order_by(Workout.date.desc()).all()
+        workout_days = set(date[0].date() for date in dates)
+        today = datetime.now(timezone.utc).date()
+        day = today
+        streak = 0
+
+        # If no workout today, skip it once
+        if day not in workout_days:
+            day -= timedelta(days=1)
+
+        # Count consecutive days with workouts
+        while day in workout_days:
+            streak += 1
+            day -= timedelta(days=1)
+    else: 
+        total_workouts = 0
+        total_minutes = 0
+        streak = 0
+    #total awards code
+    awards = UserAward.query.filter_by(user_id=user_id).count()
+
+    return jsonify(streak=streak, total_workouts=total_workouts, total_minutes=total_minutes, awards=awards)
+    
+
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
@@ -110,7 +241,6 @@ def login():
 
         if user and check_password_hash(user.password, password):
             session["user"] = user.id
-            flash("Login successful!")
             return redirect(url_for("user_page"))
         else:
             flash("Invalid credentials.")
@@ -178,11 +308,8 @@ def send_friend_request(receiver_id):
         status = "pending"
     )
         # Don't allow duplicates
-        existing_request = FriendRequest.query.filter_by(
-        requester_id=user_id,
-        receiver_id=receiver_id,
-        status="pending"
-        ).first()
+        existing_request = FriendRequest.query.filter(((FriendRequest.requester_id == user_id) & (FriendRequest.receiver_id == receiver_id)) | ((FriendRequest.requester_id == receiver_id) & (FriendRequest.receiver_id == user_id))).first()
+
 
         if existing_request:
             return jsonify(success=False, message="Friend request already sent.")
@@ -223,8 +350,15 @@ def remove_friend(friend_id):
     if friend in user.friends:
         user.friends.remove(friend)
         db.session.commit()
-        return jsonify(success=True, new_label="Add Friend", new_status="none")
     
+    request = FriendRequest.query.filter(
+    ((FriendRequest.requester_id == user_id) & (FriendRequest.receiver_id == friend_id)) |
+    ((FriendRequest.requester_id == friend_id) & (FriendRequest.receiver_id == user_id))).first()
+
+    if request:
+        db.session.delete(request)  
+        db.session.commit()
+        return jsonify(success=True, new_label="Add Friend", new_status="none")
     if not user or not friend:
         return jsonify(success=False, message="User not found")
 
@@ -303,14 +437,26 @@ def show_friend_requests():
 def showrequest():
     return render_template("friend_requests.html")
 
-@app.route("/profile")
-def profile():
-    # Get the full User object
-    user_id = session["user"]
-    user = User.query.get(user_id) 
-    image_file = url_for('static', filename='proile_pics/' + user.image_file)
+@app.route("/friends", methods=["GET","POST"])
+def view_friends():
+    return render_template("friends.html")
 
-    return render_template('profile.html', image_file=image_file)
+@app.route("/api_friends", methods=["GET","POST"])
+def api_view_friends():
+    user_id = session["user"]
+    user = User.query.get(user_id)
+    friends = user.friends
+
+    friends_list = []
+    for friend in friends:
+        friends_list.append({
+            "id": friend.id,
+            "first_name": friend.first_name,
+            "last_name": friend.last_name,
+            "status": "accepted" 
+        })
+    return jsonify(friends=friends_list)
+
 
 def save_picture(form_picture):
     #do this so the name of the users pic doesn't accidentally colide with an image already in folder
@@ -328,48 +474,189 @@ def save_picture(form_picture):
     i.thumbnail(output_size)
     #save to picture_path
     i.save(picture_path)
-    
+
     return picture_filename
 
-@app.route("/updateProfile", methods=["GET", "POST"])
+@app.route("/update_profile", methods=["GET", "POST"])
 def update_profile():
      # Get the full User object
     user_id = session["user"]
     user = User.query.get(user_id) 
-    form = UpdateAccountForm()
+    form = UpdateProfileForm()
 
     #pre-fill with current account data
     if request.method == "GET":
         form.first_name.data = user.first_name
         form.last_name.data = user.last_name
         form.image_file.data = user.image_file 
+        return render_template("update_profile.html", form=form)
 
     if form.validate_on_submit():
-        if form.picture.data:
-            picture_file = save_picture(form.picture.data)
+        if form.image_file.data:
+             #Delete old profile picture (if not default)
+            if user.image_file != "default.jpg":  
+                old_picture_path = os.path.join(app.root_path, 'static/profile_pics', user.image_file) #builds the file path
+                if os.path.exists(old_picture_path): #check if file exists
+                    os.remove(old_picture_path) 
+            #save new picture
+            picture_file = save_picture(form.image_file.data)
             user.image_file = picture_file 
+
+        # Update names
+        user.first_name = form.first_name.data
+        user.last_name = form.last_name.data
 
         #update password - user enters old password first for security 
         # Check if the current password is correct
-        if not check_password_hash(user.password, form.current_password.data):
-            flash("Incorrect current password.", "danger")
-            return redirect(url_for("update_profile"))
-        
         if form.password.data:
+            if not check_password_hash(user.password, form.current_password.data):
+                flash("Incorrect current password.", "danger")
+                return redirect(url_for("update_profile"))
+        
             if form.password.data == form.confirm_password.data:  
                 hashed_password = generate_password_hash(form.password.data)
                 user.password = hashed_password
+        
             else:
-                flash("Passwords do not match.", "danger")
-                return redirect(url_for("update_profile")) 
-
+                return redirect(url_for("update_profile"))
+                
         db.session.commit()
+
+        return redirect(url_for("user_page"))  # Redirect to a page (like profile page)
+    else:
+        return render_template("update_profile.html", form=form)
+
+@app.route("/set_timezone", methods=["POST"])
+def set_timezone():
+    if "user" not in session:
+        return "Unauthorized", 401
+    
+    tz = request.json.get("timezone")
+    user = User.query.get(session["user"])
+    if tz:
+        user.timezone = tz
+        db.session.commit()
+        now = datetime.now()
+        formatted = now.strftime("%Y-%m-%dT %I:%M %p")
+        print(formatted)
+    return "", 204 #returns no content, just a silent update 
+
+
+@app.route("/get_local_time")
+def get_local_time():
+    if "user" not in session:
+        return "Unauthorized", 401
+    
+    user = User.query.get(session["user"])
+    utc_time = datetime.now(timezone.utc)
+    if user.timezone:
+        local_time = utc_time.astimezone(ZoneInfo(user.timezone))
+        return jsonify({"local_time": local_time.strftime("%Y-%m-%dT %I:%M %p")}) #converts to string
+
+@app.route("/add_workout", methods=["GET", "POST"])
+def add_workout():
+    if request.method == "POST":
+        user_id = session["user"]
+
+        exercise=request.form["exercise"]
+        hours = int(request.form.get("hours", 0))
+        minutes = int(request.form.get("minutes", 0))
+        seconds = int(request.form.get("seconds", 0))
+        duration_minutes = hours * 60 + minutes + (seconds / 60)
+        workout_title=request.form["workout_title"]
+        date_str = request.form["datetime"]
+        date = datetime.strptime(date_str, "%Y-%m-%dT%H:%M") #converts to date/time
         
 
-        flash("Your account has been updated!", "success")
-        return redirect(url_for("profile"))  # Redirect to a page (like profile page)
+        if not date_str:
+            flash("Date and time are required.")
+            return redirect(url_for("add_workout"))
+
+
+        new_workout = Workout(user_id=user_id, exercise=exercise, duration_minutes=duration_minutes, workout_title=workout_title, date=date)
+        db.session.add(new_workout)
+        db.session.commit()
+        return redirect(url_for("user_page"))
     
-    return render_template("update_profile.html", form=form)
+    else:
+        return render_template("add_workout.html")
+
+@app.route("/view_workouts")
+def view_workouts():
+    return render_template("view_workouts.html")
+
+@app.route("/api_view_workouts")
+def api_view_workouts():
+    if "user" not in session:
+        return jsonify({"error": "Not logged in"})
+    
+    user_id = session.get("user")
+    #Find all Workout entries where user_id matches the logged-in user's ID.
+    workouts = Workout.query.filter_by(user_id=user_id).all()
+
+    workouts_data = []
+    for workout in workouts:
+        workouts_data.append({
+            "id": workout.id,
+            "user_id": workout.user_id,
+            "exercise": workout.exercise,
+            "workout_title": workout.workout_title,
+            "duration_minutes": workout.duration_minutes,
+            #this is needed to ensure the corrrect utc string is passed
+            "date": workout.date.astimezone(timezone.utc).isoformat()
+    })
+
+    return jsonify(workouts=workouts_data)
+
+@app.route("/edit_workout/<int:workout_id>", methods=["GET","POST"])
+def edit_workout(workout_id):
+    workout = Workout.query.get(workout_id)
+    form = UpdateWorkoutForm()
+
+    #pre-fill with current account data
+    if request.method == "GET":
+        form.title.data = workout.workout_title
+        form.exercise.data = workout.exercise
+        form.date.data = workout.date 
+        total_seconds = int(workout.duration_minutes * 60)
+        form.hours.data = total_seconds // 3600
+        form.minutes.data = (total_seconds % 3600) // 60
+        form.seconds.data = total_seconds % 60
+        
+        return render_template("edit_workout.html", form=form, workout_id=workout_id)
+    
+    if form.validate_on_submit():
+        if form.update.data:
+            print("Form validated and submitted")
+            workout.title = form.title.data
+            workout.exercise = form.exercise.data
+            workout.date = form.date.data
+            hours = form.hours.data or 0
+            minutes = form.minutes.data or 0
+            seconds = form.seconds.data or 0
+
+            # Convert everything to total minutes
+            total_minutes = hours * 60 + minutes + (seconds / 60)
+            #gives 2 decimal places 
+            workout.duration_minutes = round(total_minutes, 2)
+
+            db.session.commit()
+            return redirect(url_for("user_page"))
+        else:
+            print("Form did not validate.")
+            print(form.errors)
+
+
+    #delete the workout 
+        if form.delete.data:
+            #this is like doing user.workouts.delete(workout)
+            db.session.delete(workout)
+            db.session.commit()
+            return redirect(url_for("user_page"))
+
+    #need to pass workout_id so it can be defined in jinja code
+    return render_template("edit_workout.html", form=form, workout_id=workout_id)
+
 
 
 if __name__ == "__main__":
--- flask/forms.py
+++ flask/forms.py
@@ -1,7 +1,32 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import SubmitField
+from wtforms import SubmitField, StringField, PasswordField, SelectField, IntegerField
+from wtforms.validators import DataRequired, Length, EqualTo, Optional, NumberRange
+from wtforms.fields import DateTimeLocalField  
 
 class UpdateProfileForm(FlaskForm):
-    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    image_file = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    first_name = StringField('First Name', validators=[DataRequired(), Length(min=1, max=30)])
+    last_name = StringField('Last Name', validators=[DataRequired(), Length(min=1, max=30)])
+    current_password = PasswordField('Current Password', validators=[Optional()])
+    password = PasswordField('New Password', validators=[Optional(), Length(min=6)])
+    confirm_password = PasswordField('Confirm Password', validators=[EqualTo('password', message="Passwords must match")])
+
     submit = SubmitField('Update')
+
+class UpdateWorkoutForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired(), Length(min=1, max=200)])
+    exercise = SelectField('Exercise', 
+        validators=[DataRequired()], 
+        choices=[
+        ("run", "Running"),
+        ("bike", "Biking"),
+        ("swim", "Swimming"),
+        ("lift", "Weight Lifting")
+    ])
+    date = DateTimeLocalField('Date and Time', validators=[DataRequired()])
+    hours = IntegerField("Hours", validators=[Optional(), NumberRange(min=0)])
+    minutes = IntegerField("Minutes", validators=[Optional(), NumberRange(min=0, max=59)])
+    seconds = IntegerField("Seconds", validators=[Optional(), NumberRange(min=0, max=59)])
+    update = SubmitField("Update")
+    delete = SubmitField("Delete")
